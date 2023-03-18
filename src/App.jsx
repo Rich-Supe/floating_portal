8e51ea37402d2c60bbc1c23bae613930142b0e47
richsupe <richsupe@cpm.org>
2023-03-18 13:04:02 -0400

    set up island

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Canvas } from "@react-three/fiber";
+import { SceneContainer } from "./SceneContainer";
+
+export default function App() {
+  return (
+    <Canvas>
+      <SceneContainer />
+    </Canvas>
+  );
+}
