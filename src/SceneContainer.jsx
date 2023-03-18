fea298f26c1ad208bf4bbda993c9f4a194b910dd
richsupe <richsupe@cpm.org>
2023-03-18 13:39:13 -0400

    add float, words, rocks

--- src/SceneContainer.jsx
+++ src/SceneContainer.jsx
@@ -1,8 +1,12 @@
-import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Environment, Float } from "@react-three/drei";
 import { Suspense } from "react";
 import { FloatingIsland } from "./FloatingIsland";
 import { FloatingRocks } from "./FloatingRocks";
 import { Portal } from "./Portal";
+import { Trees } from "./Trees";
+import { Words } from "./Words";
+import { Grass } from "./Grass";
+
 
 export function SceneContainer() {
     return (
@@ -13,9 +17,19 @@ export function SceneContainer() {
             <PerspectiveCamera makeDefault fov={50} position={[-1.75, 10.85, 20.35]} />
             <OrbitControls target={[1, 5, 0]} maxPolarAngle={Math.PI * 0.5}/>
 
-            <FloatingIsland />
+            <Float
+                speed={0.5}
+                rotationIntensity={0.7}
+                floatIntensity={0.7}
+            >
+                <FloatingIsland />
+                <Portal />
+                <Trees />
+                <Words />
+                <Grass />
+            </Float>
+
             <FloatingRocks />
-            <Portal />
         </Suspense>
     )
 }
